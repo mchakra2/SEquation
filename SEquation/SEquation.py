8b8d4574c4140eefeee7ba2e790d55ead46df8ac
Maghesree Chakraborty <mchakra2@ur.rochester.edu>
2016-11-09 22:22:40 -0500

    Added methods to get basis set coefficients for Legendre Polynomial

--- SEquation/SEquation.py
+++ SEquation/SEquation.py
@@ -3,4 +3,49 @@ import math
 import numpy as np
 import os
 class Schrodinger:
+     #default values of parameters
      input_f='./IOFiles/input.txt'#default input file location
+     Pot_file='./IOFiles/Pot_Example.txt'
+     c=1
+     basis_size=14
+     basis_set=4
+     stat_potential=3#Since we were asked to treat potential as constant
+     o_file="./IOFiles/output.txt"
+     
+     
+     #Reads the input file and assigns the parameter values
+     def parameters(self,in_file):
+        if os.path.exists(in_file)!= True:
+            print ("The input file path does not exist. Default values will be used")
+            raise IOError
+        f = open(in_file)
+        
+        for line in f:
+
+            if "=" in line:
+                if line.split("=")[0]=='c':
+                    self.c=int(line.split("=")[1])
+                elif line.split("=")[0]=='basis_set':
+                    self.basis_set=int(line.split("=")[1])
+                elif line.split("=")[0]=='size':
+                    self.basis_size=int(line.split("=")[1])
+
+                elif line.split("=")[0]=='Pot_file':
+                    self.Pot_file=line.split("=")[1].rstrip()
+
+                elif line.split("=")[0]=='o_file':
+                    self.o_file=line.split("=")[1].rstrip()
+                    '''if os.path.exists(self.o_file)== True:
+                        warnings.warn("A file named "+self.o_file +" already exists. Overwriting the file with output")
+     data= np.loadtxt(Pot_file)'''
+     #Takes in the psi function as an array
+     def legendre_coeffs(self,psi,x):
+          self.coeff=np.polynomial.legendre.legfit(x,psi,self.basis_size)
+
+     #Takes in the legendre coefficient array and return the modified coefficient array after applying hamilton operator on it
+     def legendre_hamiltonian_coeffs(self,coefficient_array):
+          new_coeff=np.polynomial.legendre.legder(coefficient_array,2)
+          
+          new_coeff=np.append(new_coeff,[0,0])#Since taking n degree derivative reduces the number of coefficients by n
+          new_coeff=(-self.c)*new_coeff+(self.stat_potential*self.coeff)
+          return(new_coeff)
--- tests/test_SEquation.py
+++ tests/test_SEquation.py
@@ -12,6 +12,7 @@ Tests for `SEquation` module.
 import sys
 import os
 import unittest
+import numpy as np
 from contextlib import contextmanager
 from click.testing import CliRunner
 
@@ -43,7 +44,35 @@ class TestSequation(unittest.TestCase):
     
     def test_input_file_exists(self):
            
-        #self.assertRaises(IOError,self.p.parameters,'random_file_which_should_not_exist.txt')#Checks if error is raised for invalid input file
+       
         self.assertTrue(os.path.exists(self.S.input_f))#Checks if the given input file exists
-        flag = os.path.getsize(self.S.input_f)
+        flag = os.path.getsize(self.S.input_f)#Checks whether the given input file is empty
         self.assertGreater(flag,0)
+
+    def test_parameters(self):
+        self.assertRaises(IOError,self.S.parameters,'random_file_which_should_not_exist.txt')#Checks if error is raised for invalid input file
+        self.S.parameters(self.S.input_f)
+        self.assertTrue(self.S.basis_set==1 or self.S.basis_set==2)#Check if the choice of basis set is valid
+
+    def test_legendre_coeffs(self):
+        x=np.arange(-1,1,0.1)
+        function=x
+        self.S.legendre_coeffs(function,x)
+        self.assertAlmostEqual(self.S.coeff[1],1,3)#The coefficient for the second term in Legendre polynomial should be 1
+        for i in range(0,self.S.basis_size+1):
+            if i!=1:
+               self.assertAlmostEqual(self.S.coeff[i],0,3)#The coefficients for all the terms except the second in Legendre polynomial should be 0
+
+    #To check if taking hamiltonian on existing coefficient gives correct set of modifies coefficients
+    def test_hamiltonian_legendre_coeffs(self):
+        x=np.arange(-1,1,0.1)
+        function=3*(x**2)
+        self.S.legendre_coeffs(function,x)
+        #print(self.S.coeff)
+        new_coeff=self.S.legendre_hamiltonian_coeffs(self.S.coeff)
+        #Just to check, modifying coefficient according to the hamiltonian operation
+        self.S.coeff[0]=self.S.stat_potential*self.S.coeff[0]+(-self.S.c*6)
+        self.S.coeff[2]=self.S.stat_potential*self.S.coeff[2]
+        for i in range(0,self.S.basis_size+1):
+            #print(self.S.coeff[i],new_coeff[i])
+            self.assertAlmostEqual(self.S.coeff[i],new_coeff[i],0,3)#The coefficients of coeff and new_coeff should match
