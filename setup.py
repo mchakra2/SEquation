9a094aa6ee3b1025bdebd9e3431364ae7f73fcc9
Maghesree Chakraborty <mchakra2@ur.rochester.edu>
2016-11-17 19:22:53 -0500

    Still build error not fixed. got rid of py3.3 env

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
